854baad54bde0c628bcf84606b1984e6efea7a8e
Abdullah149081 <masudmd425@gmail.com>
2023-04-05 14:03:27 +0600

    header mobile responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="sticky top-0 bg-warning ">
-      <nav className="text-2xl py-8 flex gap-4 justify-center text-center ">
-        <div className="font-medium flex gap-4">
+      <nav className="text-2xl py-8 flex gap-4 justify-center text-center mb-12">
+        <div className="font-medium flex gap-4 text-lg md:text-2xl">
           <Link to="/">Home</Link>
           <Link to="/user">User</Link>
           <Link to="/posts">Posts</Link>
--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -8,7 +8,7 @@ const UserDetails = () => {
   return (
     <div className="md:px-32">
       <h2 className="text-5xl font-bold text-center">Here user Details</h2>
-      <div className="border-2 w-2/4 mx-auto border-amber-700  p-5 rounded-lg space-y-4 mt-10 pl-12">
+      <div className="border-2 md:w-2/4 mx-auto border-amber-700  p-5 rounded-lg space-y-4 mt-10 md:pl-12">
         <h2 className="text-4xl font-bold">Name: {name}</h2>
         <p className="text-2xl text-pink-500 font-medium">
           Email: <span className="cursor-pointer">{email}</span>
