d49a6680ccbc478f1f8f16b8c581540f83a17aea
Abdullah149081 <masudmd425@gmail.com>
2023-04-05 15:08:09 +0600

    go back navigate

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
   const post = useLoaderData();
   const { body } = post;
+  const navigate = useNavigate();
+  const handlerGoBck = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="md:px-32 mt-12">
-     <div className="border-2 border-gray-700 rounded-lg p-5">
-     <h2 className="text-4xl font-bold ">Description: {body}</h2>
-     </div>
+      <div className="border-2 border-gray-700 rounded-lg p-5">
+        <h2 className="text-4xl font-bold ">Description: {body}</h2>
+      </div>
+      <button onClick={handlerGoBck} className="mt-4">
+        Go back
+      </button>
     </div>
   );
 };
