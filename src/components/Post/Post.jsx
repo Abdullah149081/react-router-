0217a475a49dc18501cabfac98c577fd8cc14318
Abdullah149081 <masudmd425@gmail.com>
2023-04-05 15:05:29 +0600

    btn navigtate

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({ post }) => {
   const { title, id } = post;
+  const navigate = useNavigate();
+
+  const handlerBtn = () => {
+    navigate(`/posts/${id}`);
+  };
+
   return (
     <div className="border-2 border-amber-700  p-5 rounded-lg space-y-4">
       <h2 className="text-4xl font-bold">Id: {id}</h2>
@@ -10,7 +16,9 @@ const Post = ({ post }) => {
       <p className="text-2xl text-pink-500 font-medium">Title: {title}</p>
 
       <p className="text-success font-bold">
-        <Link to={`/posts/${id}`}>Show me Details</Link>
+        <button onClick={handlerBtn} className="btn">
+          Show me details
+        </button>
       </p>
     </div>
   );
