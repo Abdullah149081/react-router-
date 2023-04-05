44bb4a540282c4e26f95fda0090c72892b5bd018
Abdullah149081 <masudmd425@gmail.com>
2023-04-05 16:07:02 +0600

    btn mt-auto

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -10,16 +10,14 @@ const Post = ({ post }) => {
   };
 
   return (
-    <div className="border-2 border-amber-700  p-5 rounded-lg space-y-4">
-      <h2 className="text-4xl font-bold">Id: {id}</h2>
-
-      <p className="text-2xl text-pink-500 font-medium">Title: {title}</p>
-
-      <p className="text-success font-bold">
-        <button onClick={handlerBtn} className="btn">
-          Show me details
-        </button>
-      </p>
+    <div className="border-2 border-amber-700 flex flex-col  p-5 rounded-lg ">
+      <div className="space-y-4 mb-6">
+        <h2 className="text-4xl font-bold">Id: {id}</h2>
+        <p className="text-2xl text-pink-500 font-medium">Title: {title}</p>
+      </div>
+      <button onClick={handlerBtn} className="btn btn-error text-gray-900 font-bold mt-auto ">
+        Show me details
+      </button>
     </div>
   );
 };
